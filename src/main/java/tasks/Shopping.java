7af496e4597ccbdb623197533fb433a24b674d60
Juan Gomez <jgomezg@intergrupo.com>
2021-08-24 10:33:51 -0500

    Subiendo cambios en el serenity conf

--- src/main/java/tasks/Shopping.java
+++ src/main/java/tasks/Shopping.java
@@ -1,32 +1,22 @@
 package tasks;
 
-import net.serenitybdd.core.pages.WebElementFacade;
-import net.serenitybdd.core.pages.WebElementFacadeImpl;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.HoverOverBy;
 import net.serenitybdd.screenplay.ensure.Ensure;
-import net.serenitybdd.screenplay.matchers.WebElementStateMatchers;
 import net.serenitybdd.screenplay.questions.Text;
-import org.openqa.selenium.WebElement;
-
-import java.util.List;
-
-import static org.hamcrest.Matchers.*;
 
 
 import static userinterface.ShoppingManagement.*;
 
 public class Shopping implements Task {
     private static String priceValue;
-    private static List<WebElement> list;
 
     @Override
     public <T extends Actor> void performAs(T actor) {
         priceValue = Text.of(PRICE).viewedBy(actor).asString().replace("$","");
-        list.add((WebElement) PRICE);
         actor.attemptsTo(
                 Ensure.that(PRICE).textValues().contains("$16.51"),
                 Ensure.that(priceValue).asADouble().isEqualTo(16.51d),
